b3a867d7cf7477894f4d2d71f12dbd641d75b97f
Johnnie Walker <mrwalker@randomsequence.com>
2020-02-25 15:08:43 +0000

    Capture manager

--- Circles/Circles.swift
+++ Circles/Circles.swift
@@ -67,6 +67,11 @@ public class Circles : NSObject {
   }
   
   public func render(commands: MTLCommandBuffer, texture: MTLTexture, circles: MTLBuffer, info: RenderInfo) {
+
+//    let captureManager = MTLCaptureManager.shared()
+//    let scope = captureManager.makeCaptureScope(commandQueue: commands.commandQueue)
+//    captureManager.startCapture(scope: scope)
+
     let computeEncoder = commands.makeComputeCommandEncoder()!
             
     var info = FrameInfo(FrameCount: uint(info.frameIndex), CircleCount: uint(circleCount))
@@ -95,5 +100,7 @@ public class Circles : NSObject {
 */
     
     computeEncoder.endEncoding()
+
+//    captureManager.stopCapture()
   }
 }
